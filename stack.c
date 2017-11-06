82475416bc06911545344aa13e05777d45e1e025
natskyge <natskyge@void.BYOD.NET.LOCAL>
2017-11-06 08:28:47 +0100

    Remove unecessary comments

--- stack.c
+++ stack.c
@@ -1,30 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Type of data, replace int as needed */
 #define type int
 
-/* An item in the stack */
 typedef struct Item {
 	type data;
 	struct Item *prev;
 } Item;
 
-/* The stack */
 typedef struct Stack {
 	Item *top;
 	size_t size;
 	size_t max;
 } Stack;
 
-/* Initialize a stack */
 Stack*
 stackInit(size_t max)
 {
-	/* Allocate memory for the stack */
 	Stack *newStack = (Stack*)malloc(sizeof(Stack));
 	
-	/* Initialize the stack for pushing */
 	newStack->top = NULL;
 	newStack->size = 0;
 	newStack->max = max;
@@ -32,29 +26,23 @@ stackInit(size_t max)
 	return newStack;
 }
 
-/* Create an item with the values passed */
 Item*
 makeItem(type data, Item *prev)
 {
-	/* Allocate memory for the item */
 	Item *newItem = (Item*)malloc(sizeof(Item));
 	
-	/* Set the values of the item */
 	newItem->data = data;
 	newItem->prev = prev;
 	
 	return newItem;
 }
 
-/* Push an item to the top of the stack */
 int
 push(type data, Stack *stack)
 {
 	if (!stack || stack->size >= stack->max) {
-		/* Error, stackoverflow or stack is null */
 		return 1;
 	} else {
-		/* Make item and change the stack accordingly */
 		Item *newItem = makeItem(data, stack->top);
 		stack->top = newItem;
 		++stack->size;
@@ -62,15 +50,12 @@ push(type data, Stack *stack)
 	}
 }
 
-/* Remove top item from stack */
 int
 pop(Stack *stack)
 {
 	if (!stack || 0 >= stack->size) {
-		/* Error, stack is empty or null*/
 		return 1;
 	} else {
-		/* Change top to the one below and free the old top from memory */
 		Item *oldTop = stack->top;
 		stack->top = stack->top->prev;
 		--stack->size;
@@ -79,18 +64,16 @@ pop(Stack *stack)
 	}
 }
 
-/* Return the data of the top item */
 type
 peek(Stack* stack)
 {
 	if (!stack || stack->size == 0) {
-		/* There is no data, returns default value of type */
+		// No data
 	} else {
 		return stack->top->data;
 	}
 }
 
-/* Free the stacks items from memory */
 int
 clearStack(Stack *stack)
 {
@@ -100,6 +83,7 @@ clearStack(Stack *stack)
 	while (stack->size > 0) {
 		pop(stack);
 	}
+	
 	return 0;
 }
 
@@ -119,7 +103,6 @@ main(void)
 	Stack *myStack = stackInit(10);
 	printf("pop return code: %d\n", pop(myStack));
 	printStack(myStack);
-
 
 	push(23, myStack);
 	printStack(myStack);
@@ -141,5 +124,5 @@ main(void)
 
 	free(myStack);
 
-	return 1;
+	return 0;
 }
